4de267a55c4ffc07fb3ea2b836461c1d540b623d
Epharas <cbetivu@gmail.com>
2017-04-06 21:30:28 +0200

    Added destroying script for cars

---
+++ Assets/Scripts/CarDestroyer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CarDestroyer : MonoBehaviour 
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Car")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
