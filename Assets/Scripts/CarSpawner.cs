9b5d1c2c9973650892415872f7e7b8af5d006d60
Epharas <cbetivu@gmail.com>
2017-04-06 10:48:58 +0200

    Add car spawning

---
+++ Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CarSpawner : MonoBehaviour 
+{
+    public GameObject carPrefab;
+    public float spawnDelay = 2f;
+    public Transform[] spawnPositions;
+
+    float spawnTimer = 0f;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnCar", 0f, spawnDelay);
+    }
+
+    void SpawnCar()
+    {
+        int index = Random.Range(0, spawnPositions.Length);
+        Transform spawnPoint = spawnPositions[index];
+        Instantiate(carPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
