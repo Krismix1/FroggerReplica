97b86ff2c2b47b4aafb93253f1cd1b09b575d74d
Epharas <cbetivu@gmail.com>
2017-04-06 22:01:05 +0200

    Added player movement restraints for bottom, left and right side

--- Assets/Scripts/Frog.cs
+++ Assets/Scripts/Frog.cs
@@ -3,25 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class Frog : MonoBehaviour 
 {
-
-    private Rigidbody2D rb2D;
-
-    private void Start()
-    {
-        rb2D = GetComponent<Rigidbody2D>();
-    }
+    public Transform leftWall;
+    public Transform rightWall;
+    public Transform bottomWall;
+    public Rigidbody2D rb2D;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
             rb2D.MovePosition(rb2D.position + Vector2.up);
-        }else if (Input.GetKeyDown(KeyCode.DownArrow))
+        }else if (Input.GetKeyDown(KeyCode.DownArrow) &&
+            rb2D.position.y + Vector2.down.y >= bottomWall.position.y)
         {
             rb2D.MovePosition(rb2D.position + Vector2.down);
-        }else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        }else if (Input.GetKeyDown(KeyCode.LeftArrow) && 
+            rb2D.position.x + Vector2.left.x >= leftWall.position.x)
         {
             rb2D.MovePosition(rb2D.position + Vector2.left);
-        }else if (Input.GetKeyDown(KeyCode.RightArrow))
+        }else if (Input.GetKeyDown(KeyCode.RightArrow) && 
+            rb2D.position.x + Vector2.right.x <= rightWall.position.x)
         {
             rb2D.MovePosition(rb2D.position + Vector2.right);
         }
